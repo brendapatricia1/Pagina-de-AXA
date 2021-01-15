26c7ca53e0955596c4feffd4c08ab449d9deade2
brendapatricia1 <brendapatricia1@aranomartinez.github.com>
2021-01-14 19:04:53 -0600

    Esta mal eso

--- coments.php
+++ coments.php
@@ -1,23 +1,11 @@
 <?php
-$conectar=@mysql_connect('localhost','axaveracruz16','axaveracruz1');
-if(!$conectar){
-    echo"No se pudo conectar con el servidor";
-}else{
-    $base=mysql_select_db('comentarios');
-    if(!$base){
-        echo"No se encontro la base de datos";
-    }
-}
+$conectar=mysql_connect("localhost","axaveracruz16","axaveracruz1") or die("No se pudo conectar el servidor");
+$base=mysql_select_db("comentarios",$conectar) or die("No se pudo encontrar la base de datos");
+
 $nombre=$_POST['nombre'];
 $telefono=$_POST['telefono'];
 $email=$_POST['email'];
 $mensaje=$_POST['mensaje'];
 
-$sql="INSERT INTO mensajes VALUES('$nombre','$telefono','$email','$mensaje')";
-$ejecutar=mysql_query($sql);
-if(!$ejecutar){
-    echo"No se ejecuto correctamente";
-}else{
-    echo"Datos Guardados Correctamente <br> <a href='contacto.html'>Regresar</a>";
-}
+mysql_query("INSERT INTO mensajes VALUES('$nombre','$telefono','$email','$mensaje', $conectar)" or die ("Error de datos");
 ?>
